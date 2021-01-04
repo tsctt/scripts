bd441c1e77faa107e3fab3d53f011c7338043ffb
Tim Schuett <schuett@juno.berlin.1blu.de>
2021-01-04 10:45:38 +0100

    Fix for missing variable + comment

--- login_notify.py
+++ login_notify.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
 
+# Send an e-mail on ssh login
+# run via /etc/ssh/sshrc
+
 import os
 import smtplib
 import email.utils
 import socket
 from email.mime.text import MIMEText
 
-notification_mail = "<YOUR_MAILADDR>"								# email address for notifications
+notification_mail = "<YOUR_EMAIL>"								# email address for notifications
 ip_whitelist = ["127.0.0.1"]									# if the login ip is one of these, no notification will be sent out
 
+global ssh_ip
+global hostname
 
-
-
-ssh_ip =	os.environ['SSH_CONNECTION'].split(" ")[0]
-ssh_user =	os.environ['USER']
-hostname = 	socket.gethostname()
+try:
+    ssh_ip =	os.environ['SSH_CONNECTION'].split(" ")[0]
+except:
+    ssh_ip =	"-"
+    
+hostname =      socket.gethostname()
+ssh_user =       os.environ['USER']
 reverse_dns = ""
 
 if ssh_ip not in ip_whitelist:
     try:
         reverse_dns = socket.gethostbyaddr(ssh_ip)[0]
     except:
-        reverse_dns = "unknown"
+        reverse_dns = "-"
 
     mail_msg =      "SSH login on " + str(hostname) + ":\n\nHost: " + str(hostname) + "\nfrom ip: " + str(ssh_ip) + " (" + str(reverse_dns) + ")\nssh user: " + str(ssh_user)
     sender_mail = "root@" + str(hostname)
